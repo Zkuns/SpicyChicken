29c2f4f555ce2c64fef97a8419275bed9c6f93be
zhukun <zhukun6150909@outlook.com>
2016-03-09 17:37:09 +0800

    Initial Commit

---
+++ SpicyChicken/ActivityCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityCell.swift
+//  SpicyChicken
+//
+//  Created by 朱坤 on 3/10/16.
+//  Copyright © 2016 Zkuns. All rights reserved.
+//
+
+import UIKit
+
+class ActivityCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
